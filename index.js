0139d30d8742e2fa4e0b2cb347a816d0f358ece4
Hussein Talal <hossientalal2@gmail.com>
2020-05-31 07:30:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 // Write your solution in this file!
 let driver = {};
+function updateDriverWithKeyAndValue(obj, key, value){
+    obj[key] = value;
+    return obj;
+}
+
+let newObj = updateDriverWithKeyAndValue(driver, "name", "hossien");
+console.log(newObj.name);
