c5610adb5eda1fbc48fb27ddf2646c58015e8cbc
Hussein Talal <hossientalal2@gmail.com>
2020-05-31 07:40:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // Write your solution in this file!
 let driver = {};
 function updateDriverWithKeyAndValue(obj, key, value){
-    obj[key] = value;
-    return obj;
+  let newObj = {...obj};
+    newObj[key] = value;
+    return newObj;
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(obj, key, value){
-  let newObj = {...obj};
+
   newObj[key] = value;
   return newObj;
 }
